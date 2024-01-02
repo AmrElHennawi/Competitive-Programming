50c4d75ef2e63b4a47a847dc2829e5e3e9dd7fdc
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-02 21:28:58 +0200

    Create 707A-Brain's-Photos.cpp

---
+++ Codeforces/707A-Brain's-Photos.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+#define mem(arrr, xx) memset(arrr, xx, sizeof arrr)
+#define PI acos(-1)
+#define int long long
+// #define debug(x) cout << (#x) << " = " << x << endl
+
+void Amr()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+}
+
+void solve()
+{
+    int n, m;
+    char color;
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> color;
+            if (color == 'C' || color == 'M' || color == 'Y')
+            {
+                cout << "#Color" << endl;
+                return;
+            }
+        }
+    }
+    cout << "#Black&White" << endl;
+}
+
+signed main()
+{
+    Amr();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
